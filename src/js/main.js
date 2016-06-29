6abf39758b316377378c9f0baef89a0cf27dbb36
Chad Beach <cb832001@gmail.com>
2016-06-29 08:35:41 -0400

    updated all info to page with style

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,33 @@
 import $ from 'jquery';
 
 $.ajax({
-  url: 'https://randomuser.me/api/',
+  url: 'http://api.randomuser.me/',
   dataType: 'json',
+  data: {
+    results: "12",
+    nat: "US",
+    inc: "name, location, email, cell, id, picture"
+  },
   success: function(data){
-    console.log(data);
+  console.log(data)
+data.results.map(create_user)
   }
 });
+
+
+var create_user = function (info) {
+  $( ".container" ).append(`
+    <div class="grid">
+      <img class="large" src="${info.picture.large}">
+      <div class="name">${info.name.first},${info.name.last}</div>
+      <div class="location">
+        ${info.location.street}<br>
+        ${info.location.city},${info.location.state}.${info.location.postcode}
+        <div class="cell">${info.cell}</div>
+      </div>
+      <div class="email">${info.email}</div>
+      <div class="id">${info.id.value}</div>
+    </div>`);
+  console.log(info);
+
+}
